5dd99ff747e8fc699ebcc5b4497b31b590e4e83c
Christopher Stone <cmstone@student.fullsail.edu>
2022-05-09 23:42:24 -0400

    Add restful API

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+const productRouter = require('./routes/Products')
 
-app.get('/products/all', (request, response) => {
-    if(JSON.stringify(request.query) === '{}') {
-        response.send('Products, ' + request.header('Page') + ', ' + request.header('Sort') + ', ' + request.header('Order'))
-    } else {
-        response.send('Products, ' + request.query.sort)
-    }
-})
-
-app.get('/products/all', (request, response) => {
-    response.send('Products, ' + request.query.sort)
-})
-
-app.get('/products/:productId-:productSize-:productColor', (request, response) => {
-    response.send('Product ,' + request.params.productId + ', ' + request.params.productSize + ', ' + request.params.productColor)
-})
-
-app.get('/products/:productId', (request, response) => {
-    response.send('Product, ' + request.params.productId)
-})
+app.use('/products', productRouter)
 
 app.listen(3000)
