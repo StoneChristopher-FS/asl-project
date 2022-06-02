d3c36dd62d19b513ff82f1daeed81585d2a48109
Christopher Stone <cmstone@student.fullsail.edu>
2022-06-02 17:36:14 -0400

    Start building front end shop

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(fileUpload())
 const productRouter = require('./routes/Products')
 const variantsRouter = require('./routes/Variants')
 const imagesRouter = require('./routes/Images')
+const storeRouter = require('./routes/Store')
+const { index } = require('./controllers/Store')
 app.set('views', __dirname + '/templates')
 app.set('view engine', 'twig')
 
-app.get('/', (req, res) => {
-    res.render('views/home')
-})
-
 app.use('/public', express.static('public'))
 
+app.get('/', index)
+app.use("/store", storeRouter)
 app.use('/products', productRouter)
 app.use('/variants', variantsRouter)
 app.use('/images', imagesRouter)
